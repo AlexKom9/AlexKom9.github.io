ffebd3bdd2ec13c592cb1a3198b0d472fd84089a
Alexandr Komar <909komar@gmail.com>
2018-03-27 16:21:32 +0300

    peiko first

---
+++ peiko/js/scroll.js
@@ -0,0 +1,12 @@
+//scroll
+$("#hero__look").click(function() {
+  $('html, body').animate({
+      scrollTop: $(window).height() }, 500);
+});
+// 
+// $(document).ready(function(){
+//   $.scrollify({
+//     section : ".section",
+//     scrollSpeed: 1500,
+//   });
+// });
