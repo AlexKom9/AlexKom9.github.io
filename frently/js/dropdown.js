0ac402e5925b83c0c5e332032b6eb32c89fb096f
Alexandr Komar <909komar@gmail.com>
2018-06-11 17:15:12 +0300

    fix frently

--- frently/js/dropdown.js
+++ frently/js/dropdown.js
@@ -13,6 +13,13 @@ function DropDown(){
 				console.log('clickOut')
 				$('.l-dropdown').hide()
 				$('.l-dropdown-box').data('status','closed')
+				$('.l-dropdown-box').each(function(){
+					$(this).data('status', 'closed')
+					if ($(this).find($('.l-dropdown__input-text-filter')).length != 0){
+						$(this).find('.l-dropdown__input-text-filter').hide()
+						$(this).find('span').show()
+					}
+				})
 			})
 		},
 		clickDropdown: function(){
@@ -23,7 +30,7 @@ function DropDown(){
 		clickDropdownBox: function(){
 			$('.l-dropdown-box').on('click', function(event){
 				event.stopPropagation();
-				// console.log('click')
+
 				var status = $(this).data('status')
 				console.log(status)
 				$('.l-dropdown').hide()
@@ -34,10 +41,23 @@ function DropDown(){
 					$(this).data('status','opened')
 					console.log(this)
 					console.log($(this).data('status'))
+					// console.log($(this).find($('.l-dropdown__input-text-filter')))
+					if ($(this).find($('.l-dropdown__input-text-filter')).length != 0){
+						console.log('----------has')
+						$(this).find('.l-dropdown__input-text-filter').show()
+						$(this).find('.l-dropdown__input-text-filter').focus()
+						$(this).find('span').hide()
+					}
 				} else {
 					console.log('closing')
 					$(this).find('.l-dropdown').hide()
 					$(this).data('status','closed')
+
+					if ($(this).find($('.l-dropdown__input-text-filter')).length != 0){
+						$(this).find('.l-dropdown__input-text-filter').hide()
+						$(this).find('span').show()
+					}
+
 				}
 			})
 		},
@@ -47,31 +67,16 @@ function DropDown(){
 				var val = $(this).html()
 				var placeholder = $(this).closest('.l-dropdown-box').data('placeholder')
 
-				// if($(this).parent().hasClass('l-dropdown__checkboxes-container')){
-				// 	var lsVal = []
-				// 	var boxVal = $(this).closest('.l-dropdown-box').children('span').html();
-				//
-				// 	$(this).parent().children('input:checked').each(function(index, elem){
-				// 		console.log($(elem).next('label').html())
-				// 		lsVal.push($(elem).next('label').html())
-				// 	})
-				//
-				// 	if(lsVal.indexOf(val) === -1){
-				// 		lsVal.push(val);
-				// 	} else {
-				// 		lsVal.splice(lsVal.indexOf(val),1)
-				// 	}
-				// 	if(lsVal.length == 0){
-				// 		// console.log('length  = 0')
-				// 		lsVal.push(placeholder)
-				// 	}
-				// 	$(this).closest('.l-dropdown-box').children('span').html(lsVal.sort().join())
-				// 	return
-				// }
+				var box = $(this).closest('.l-dropdown-box');
 
-				$(this).closest('.l-dropdown-box').data('status','closed')
-				$(this).closest('.l-dropdown-box').children('span').html(val)
-				$(this).closest('.l-dropdown-box').data('filled', true)
+				if ($(box).find($('.l-dropdown__input-text-filter')).length != 0){
+					$(box).find('.l-dropdown__input-text-filter').hide()
+					$(box).find('span').show()
+				}
+
+				$(box).data('status','closed')
+				$(box).children('span').html(val)
+				$(box).data('filled', true)
 				$(this).closest('.l-dropdown').hide()
 
 			})
@@ -129,10 +134,35 @@ function DropDown(){
 				})
 			})
 		},
+		inputTextFilter: function(){
+			$('.l-dropdown__input-text-filter').on('click', function(event){
+				event.stopPropagation()
+				var parent = $(this).parent('.l-dropdown-box')
+				$('.l-dropdown-box').data('status', 'closed')
+				$('.l-dropdown').hide()
+				parent.data('status', 'opened')
+				parent.find('.l-dropdown').show()
+			})
+			$('.l-dropdown__input-text-filter').on('keyup', function(){
+				// console.log('---on change')
+				var key =  $(this).val().toLowerCase()
+				$(this).parent('.l-dropdown-box').find('.dropdown-chose-item').each(function(){
+					var city = $(this).html().toLowerCase()
+					// console.log(city)
+					console.log(city.includes(key))
+					if (city.includes(key)){
+						$(this).show()
+					} else {
+						$(this).hide()
+					}
+				})
+			})
+		},
 		initialState(){
 			$('input[type=checkbox]').each(function(){
 				this.checked = false
 			})
+			$('.l-dropdown__input-text-filter').hide()
 			// $('.l-dropdown-box').each(function(){
 			// 	// $(this).data('filled', false)
 			// })
@@ -144,6 +174,7 @@ function DropDown(){
 			this.onCheckBoxChange();
 			this.clickOut();
 			this.initialState()
+			this.inputTextFilter()
 			this.resetInput()
 		}
 	}
